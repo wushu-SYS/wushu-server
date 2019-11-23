d1a37da25b703403d72fcad7b9a13cb0377d5ffd
dror <droruri3@gmail.com>
2019-11-23 19:01:10 +0200

    *change function server side -> get coach by ID

--- implementation/common/couches_module.js
+++ implementation/common/couches_module.js
@@ -14,12 +14,12 @@ async function getCoaches() {
 
 async function getCoachProfileById(id) {
     let ans = new Object();
-    await dbUtils.sql(`Select id, firstname, lastname, sportclub from user_Coach Where id= @idCoach`)
+    await dbUtils.sql(`Select * from user_Coach Where id= @idCoach`)
         .parameter('idCoach', tediousTYPES.Int, id)
         .execute()
         .then(function (results) {
             ans.status = Constants.statusCode.ok;
-            ans.results = results
+            ans.results = results[0]
         }).fail(function (err) {
             ans.status = Constants.statusCode.badRequest;
             ans.results = err;
