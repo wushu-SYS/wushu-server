98d9f3103abc5fe392076d4faa662d61434a4caf
droru <droruri3@gmail.com>
2019-12-04 10:37:45 +0200

    *excel creation for register new judge *excel creation for register coach as judge

--- implementation/services/excelCreation.js
+++ implementation/services/excelCreation.js
@@ -476,6 +476,7 @@ async function createExcelCoachAsJudge(coachList) {
 
 }
 
+
 async function createExcelCompetitionState(compState, date) {
     let workbook = new excel.Workbook();
     workbook.writeP = util.promisify(workbook.write);
