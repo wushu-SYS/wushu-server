d1aaae51a6ce2589160d8a2626e362327fe189d3
dror <droruri3@gmail.com>
2019-12-06 13:59:05 +0200

    * create function for reating code ->create Excel

--- app.js
+++ app.js
@@ -520,6 +520,7 @@ app.post("/private/commonCoachManager/updateCoachProfile", async function (req, 
             res.status(Constants.statusCode.badRequest).send(Constants.errorMsg.accessDenied)
     }
 );
+/*
 app.post("/private/manager/registerNewJudge", async function (req, res) {
     let ans;
     ans = manager_judge_module.checkJudgeDataBeforeRegister(common_function.getArrayFromJson(req.body));
@@ -530,6 +531,8 @@ app.post("/private/manager/registerNewJudge", async function (req, res) {
         res.status(Constants.statusCode.badRequest).send(ans.errors)
 })
 
+
+ */
 
 //start the server
 app.listen(process.env.PORT || 3000, () => {
