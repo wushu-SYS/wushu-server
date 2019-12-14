3efc59eda122849e9a707dc06a58df996195e277
alina <alinapolei96@gmail.com>
2019-12-14 10:48:04 +0200

    UC 12 add new club server side

--- implementation/manger/sportClub_module.js
+++ implementation/manger/sportClub_module.js
@@ -5,11 +5,11 @@ function validateSportClubDetails(data){
     ans.isPassed = true;
     let err = [];
     //clubName
-    if (!validator.matches(data.clubName, Constants.regexHebWithSpace))
-        err.push(Constants.errorMsg.hebErr)
-    //address
-    if (!validator.matches(data.address, Constants.regexHebrewAndNumbers))
-        err.push(Constants.errorMsg.hebErr)
+    // if (!validator.matches(data.clubName, Constants.regexHebWithSpace))
+    //     err.push(Constants.errorMsg.hebErr)
+    // //address
+    // if (!validator.matches(data.address, Constants.regexHebrewAndNumbers))
+    //     err.push(Constants.errorMsg.hebErr)
 
 
     if (err.length != 0)
@@ -26,7 +26,7 @@ async function addSportClub(data){
         .parameter('phone', tediousTYPES.NVarChar, data.phone)
         .parameter('address', tediousTYPES.NVarChar, data.address)
         .parameter('contactname', tediousTYPES.NVarChar, data.contactname)
-        .parameter('amutaId', tediousTYPES.Int, data.amutaId)
+        .parameter('amutaId', tediousTYPES.Int, data.amutaId ? data.amutaId : 999999999)
         .parameter('agudaId', tediousTYPES.Int, data.agudaId)
         .parameter('ergonId', tediousTYPES.Int, data.ergonId)
         .execute()
