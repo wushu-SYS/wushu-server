42ba1a593ca1f578a7739890b1fcaec3d10ba870
alina <alinapolei96@gmail.com>
2019-12-14 20:19:12 +0200

    UC 7 update judge profile

--- implementation/common/judge_module.js
+++ implementation/common/judge_module.js
@@ -40,6 +40,28 @@ async function getRefereeProfileById(id) {
         });
     return ans;
 }
+
+async function updateRefereeProfile(CoachData) {
+    let ans = new Object();
+    await dbUtils.sql(`UPDATE user_Judge SET id =@id, firstname = @firstName, lastname = @lastName, phone = @phone, email = @email where id =@oldId;`)
+        .parameter('id', tediousTYPES.Int, CoachData[0])
+        .parameter('firstName', tediousTYPES.NVarChar, CoachData[1])
+        .parameter('lastName', tediousTYPES.NVarChar, CoachData[2])
+        .parameter('phone', tediousTYPES.NVarChar, CoachData[3])
+        .parameter('email', tediousTYPES.NVarChar, CoachData[4])
+        .parameter('oldId', tediousTYPES.Int, CoachData[5])
+        .execute()
+        .then(function (results) {
+            ans.status = Constants.statusCode.ok;
+            ans.results = Constants.msg.updateUserDetails;
+        }).fail(function (err) {
+            ans.status = Constants.statusCode.badRequest;
+            ans.results = err
+        });
+    return ans;
+}
+
 module.exports.getReferees = getReferees;
 module.exports.getJudgesToRegister = getJudgesToRegister;
 module.exports.getRefereeProfileById = getRefereeProfileById;
+module.exports.updateRefereeProfile = updateRefereeProfile;
